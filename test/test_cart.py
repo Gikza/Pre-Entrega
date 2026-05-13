30a2229273084b8e08e8035a1d2580f7607cac5d
Gikza Carmona <gikcarmona@gmail.com>
2026-05-12 21:39:39 -0300

    primer commit

---
+++ test/test_cart.py
@@ -0,0 +1,27 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import pytest
+
+def test_cart(login_in_driver):
+    driver = login_in_driver
+
+    # Agregar producto al carrito
+    driver.find_elements(By.CLASS_NAME, "btn_inventory")[0].click()
+
+    contador_cart = driver.find_element(By.CLASS_NAME, "shopping_cart_badge")
+
+# Verificar contador carrito
+    assert contador_cart.text == "1" "El producto no se agregó correctamente"
+
+    # Obtener nombre del primer producto
+    product_name = driver.find_elements(By.CLASS_NAME, "inventory_item_name")[0].text
+
+# Ir al carrito
+    driver.find_element(By.CLASS_NAME, "shopping_cart_link").click()
+
+    # Obtener el nombre del producto en el carrito
+    cart_item = driver.find.element(By.CLASS_NAME, "inventory_item_name")
+
+    # Verificar el producto agregado en el carrito
+
+    assert cart_item == product_name, "El producto agregado no coincide"
