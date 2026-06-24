b3d0cd9edfdd104909c1611821ca52c5bfdf256d
Gikza Carmona <gikcarmona@gmail.com>
2026-06-24 16:14:08 -0300

    Fix: sintaxis f-string en test_cart_json

--- test/test_cart_json.py
+++ test/test_cart_json.py
@@ -27,6 +27,6 @@ def test_cart_json(driver_logged):
         print(productos_carrito)
         print(productos)
 
-        assert encontrado, f"Producto incorrecto o faltante: {producto_json["nombre"]}"        
+        assert encontrado, f"Producto incorrecto o faltante: {producto_json['nombre']}"        
      
                    
